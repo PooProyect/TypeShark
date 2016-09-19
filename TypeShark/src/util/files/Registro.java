3934369d95a68c5191f2474553c216f41cb6efcc
carangar <carlos.garzon@hotmail.es>
2016-09-18 19:00:17 -0500

    correccion del anterior, tenia ciertos errores  de compilacion

--- TypeShark/src/util/files/Registro.java
+++ TypeShark/src/util/files/Registro.java
@@ -8,6 +8,7 @@ package util.files;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
